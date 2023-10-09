276cc0a8df5d876cb414983e36da702c4521a316
ellietsamouli <elli.tsamouli@edu.futuregames.nu>
2023-10-09 18:46:20 +0200

    amlost made a succesful animation

---
+++ The3Cs/Assets/Scripts/AnimationPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPlayer : MonoBehaviour
+{
+    int isWalkingHash;
+    Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+       
+        isWalkingHash = Animator.StringToHash("isWalking");
+    }
+    private void Update()
+    { 
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKeyDown("w");
+        if (!isWalking &&  forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, true);
+        }
+
+        if (isWalking &&!forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, false);
+        }
+    }
+}
