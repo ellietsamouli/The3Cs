0b21d6489a9a6c27ddf489ec8a63dea3f09fb333
ellietsamouli <elli.tsamouli@edu.futuregames.nu>
2023-10-10 14:15:51 +0200

    made jump, pick up set up
    need to connect script to the pick up, have an animation etc

--- The3Cs/Assets/Scripts/AnimationPlayer.cs
+++ The3Cs/Assets/Scripts/AnimationPlayer.cs
@@ -4,26 +4,55 @@ using UnityEngine;
 
 public class AnimationPlayer : MonoBehaviour
 {
-    int isWalkingHash;
-    Animator animator;
+    private Animator animator;
+    private int isRunningHash;
+    private int isWalkingHash;
+    private int isJumpingHash;
+
     private void Start()
     {
         animator = GetComponent<Animator>();
-       
+        isRunningHash = Animator.StringToHash("isRunning");
         isWalkingHash = Animator.StringToHash("isWalking");
+        isJumpingHash = Animator.StringToHash("isJumping");
     }
+
     private void Update()
-    { 
+    {
+        bool isRunning = animator.GetBool(isRunningHash);
         bool isWalking = animator.GetBool(isWalkingHash);
         bool forwardPressed = Input.GetKey("w");
-        if (!isWalking &&  forwardPressed)
+        bool runPressed = Input.GetKey("left shift");
+        bool jumpPressed = Input.GetKeyDown("space");
+
+        if (!isWalking && forwardPressed)
         {
             animator.SetBool(isWalkingHash, true);
         }
 
-        if (isWalking &&!forwardPressed)
+        if (isWalking && !forwardPressed)
         {
             animator.SetBool(isWalkingHash, false);
+        }
+
+        if (!isRunning && (forwardPressed && runPressed))
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+
+        if (isRunning && (!forwardPressed || !runPressed))
+        {
+            animator.SetBool(isRunningHash, false);
+        }
+
+        // Check for jump input and set the isJumping parameter accordingly.
+        if (jumpPressed)
+        {
+            animator.SetBool(isJumpingHash, true);
+        }
+        else
+        {
+            animator.SetBool(isJumpingHash, false);
         }
     }
 }
