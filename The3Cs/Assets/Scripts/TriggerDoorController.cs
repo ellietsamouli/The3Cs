588e47e0b10e1b9cbc16b46724fcc96dc8306401
ellietsamouli <elli.tsamouli@edu.futuregames.nu>
2023-10-11 17:33:54 +0200

    Open door animation works!Like shit but works

---
+++ The3Cs/Assets/Scripts/TriggerDoorController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoorController : MonoBehaviour
+{
+    [SerializeField] private Animator myDoor = null;
+
+    [SerializeField] private bool openTrigger = false;
+    [SerializeField] private bool closeTrigger = false;
+    [SerializeField] private string doorOpen = "DoorOpen";
+    [SerializeField] private string doorClose = "DoorClose";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (openTrigger)
+            {
+                myDoor.Play("DoorOpen", 0, 0.0f);
+                gameObject.SetActive(true);
+            }
+            else if (closeTrigger)
+            {
+                myDoor.Play("DoorClose", 0, 0.0f);
+                gameObject.SetActive(true);
+            }
+        }
+    }
+
+}
