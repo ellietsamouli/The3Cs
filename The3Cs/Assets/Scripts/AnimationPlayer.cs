f68d6601dc7d7366e4dcfe92351da5260a086ea7
ellietsamouli <elli.tsamouli@edu.futuregames.nu>
2023-10-11 11:07:36 +0200

    now when the player has the key they can open doors with animations

--- The3Cs/Assets/Scripts/AnimationPlayer.cs
+++ The3Cs/Assets/Scripts/AnimationPlayer.cs
@@ -9,14 +9,18 @@ public class AnimationPlayer : MonoBehaviour
     private int isWalkingHash;
     private int isJumpingHash;
     private int isLiftingHash;
+    private int isOpeningHash; // The new animation parameter
+
+    private bool hasKey = false;
 
     private void Start()
     {
-        animator = GetComponent<Animator>();
+        animator = GetComponent < Animator>();
         isRunningHash = Animator.StringToHash("isRunning");
         isWalkingHash = Animator.StringToHash("isWalking");
         isJumpingHash = Animator.StringToHash("isJumping");
         isLiftingHash = Animator.StringToHash("isLifting");
+        isOpeningHash = Animator.StringToHash("isOpening"); // Assign the hash for the new parameter
     }
 
     private void Update()
@@ -63,21 +67,45 @@ public class AnimationPlayer : MonoBehaviour
 
         // Pick-up animation logic
         bool isLifting = animator.GetBool(isLiftingHash);
-        bool pickUpPressed = Input.GetKey("e");
+        bool pickUpPressed = Input.GetKey("c");
 
         if (pickUpPressed && !isLifting)
         {
             // Set the "isLifting" parameter to true to trigger the pick-up animation.
             animator.SetBool(isLiftingHash, true);
-
-            // Debug the key press.
-            Debug.Log("E key pressed.");
         }
         else if (!pickUpPressed && isLifting)
         {
-            // Reset the "isLifting" parameter to false when the "E" key is released.
+            // Reset the "isLifting" parameter to false when the "C" key is released.
             animator.SetBool(isLiftingHash, false);
         }
 
+        // Check if the player has the key
+        if (hasKey)
+        {
+            // Opening animation logic
+            bool isOpening = animator.GetBool(isOpeningHash);
+            bool openPressed = Input.GetKey("o");
+
+            if (openPressed && !isOpening)
+            {
+                // Set the "isOpening" parameter to true to trigger the opening animation.
+                animator.SetBool(isOpeningHash, true);
+            }
+            else if (!openPressed && isOpening)
+            {
+                // Reset the "isOpening" parameter to false when the "O" key is released.
+                animator.SetBool(isOpeningHash, false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Key"))
+        {
+            // If the player collides with an object tagged "Key," set hasKey to true.
+            hasKey = true;
+        }
     }
 }
