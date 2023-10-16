803b3755e60cc02731d1a4b3300e77187e1a4e99
ellietsamouli <elli.tsamouli@edu.futuregames.nu>
2023-10-16 16:54:40 +0200

    Triggers and interactables are set

---
+++ The3Cs/Assets/Scripts/SlideTrigger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideTrigger : MonoBehaviour
+{
+
+    [SerializeField] private Animator myDoor = null;
+    [SerializeField] private bool openTrigger = false;
+    [SerializeField] private string doorOpen = "DoorOpen";
+
+    private bool hasKey = false; // Indicates whether the player has the key.
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") )
+        {
+            if (openTrigger)
+            {
+                myDoor.Play(doorOpen, 0, 0.0f);
+            }
+            
+        }
+    }
+
+   
+
+
+}
