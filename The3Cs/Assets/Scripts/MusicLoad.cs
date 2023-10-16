11d030fd2195438d19a0ddbcec3f81353f8838e0
ellietsamouli <elli.tsamouli@edu.futuregames.nu>
2023-10-16 21:35:22 +0200

    ALL DONE

--- The3Cs/Assets/Scripts/MusicLoad.cs
+++ The3Cs/Assets/Scripts/MusicLoad.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class MusicLoad : MonoBehaviour
 {
-    
+    private static MusicLoad instance;
+
+    void Start()
+    {
+        // Check if an instance of AudioManager already exists.
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // This line keeps the object alive between scenes.
+        }
+        else
+        {
+            // If an instance already exists, destroy this object.
+            Destroy(gameObject);
+        }
+    }
+
 
 }
